66f46801697562ee6b625bdce659de5ce6688451
Obilomania <iloanyaobinna@gmail.com>
2022-12-29 22:58:38 +0100

    implemented seacrh function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { RiH1 } from "react-icons/ri";
+import styled from "styled-components";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [editTodoData, setEditTodoData] = useState(null);
+  const [searchTodo, setSearchTodo] = useState("");
   const handleTodos = (getLatestTodo) => {
     const newTodo = [...todos];
     const indexOfLatestTodoItem = newTodo.findIndex(
@@ -20,7 +23,19 @@ const TodoList = () => {
       };
     }
     setTodos(newTodo);
+    localStorage.setItem("todoList", JSON.stringify(newTodo));
   };
+
+  const handleSearch = (event) => {
+    const { value } = event.target;
+    setSearchTodo(value.toLowerCase());
+  };
+
+  const filterTodo =
+    todos && todos.length
+      ? todos.filter((item) => item.text.toLowerCase().includes(searchTodo))
+      : [];
+
   const editTodo = (editedData) => {
     setEditTodoData(editedData);
   };
@@ -29,13 +44,62 @@ const TodoList = () => {
     let getTodos = [...todos];
     getTodos = getTodos.filter((item) => item.id !== deletebyId);
     setTodos(getTodos);
+    localStorage.setItem("todoList", JSON.stringify(getTodos));
   };
+
+  useEffect(() => {
+    const gettodosfromLocalStorage = JSON.parse(
+      localStorage.getItem("todoList")
+    );
+    if (gettodosfromLocalStorage && gettodosfromLocalStorage.length) {
+      setTodos(gettodosfromLocalStorage);
+    }
+  }, []);
   return (
-    <>
+    <AppJS>
+      <div className="searchTodo">
+        <label htmlFor="">Search todo</label>
+        <input
+          placeholder="Search Todo"
+          type="text"
+          name="search"
+          onChange={handleSearch}
+          value={searchTodo}
+        />
+      </div>
       <TodoForm getNewTodoItem={handleTodos} editTodoData={editTodoData} />
-      <TodoItem todos={todos} editTodo={editTodo} getTodobyId={deleteTodo} />
-    </>
+      {filterTodo && filterTodo.length ? (
+        <TodoItem
+          todos={filterTodo}
+          editTodo={editTodo}
+          getTodobyId={deleteTodo}
+        />
+      ) : (
+        <h5>
+          No <b>To-do</b> Found!!!{" "}
+        </h5>
+      )}
+    </AppJS>
   );
 };
+
+const AppJS = styled.div`
+  width: 100%;
+  .searchTodo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+  }
+  .searchTodo input {
+    width: 30%;
+    padding: 0.2rem 1rem;
+    border-radius: 1rem;
+  }
+  h5 {
+    text-align: center;
+    color: red;
+  }
+`;
 
 export default TodoList;
